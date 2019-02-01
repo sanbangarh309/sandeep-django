ad657113626fe14b9c83e4e1727d19ad393824e1
Sandeep Bangarh <sanbangarh309@gmail.com>
2019-02-01 13:34:57 +0530

    migration added

--- sandeep/views.py
+++ sandeep/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Users
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
-
 def db(request):
-
     user = Users()
     user.save()
 
