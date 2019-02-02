c034f2ddb0945de35f28333c803a2db6c18a2419
Sandeep Bangarh <sanbangarh309@gmail.com>
2019-02-02 22:23:41 +0530

    django admin changed

--- sandeep/migrations/0004_auto_20190202_1449.py
+++ sandeep/migrations/0004_auto_20190202_1449.py
@@ -10,12 +10,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='project',
-            name='id',
-            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
-            preserve_default=False,
-        ),
         migrations.AlterField(
             model_name='project',
             name='about',
