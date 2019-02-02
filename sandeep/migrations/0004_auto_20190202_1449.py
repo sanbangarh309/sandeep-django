7af88877f0c60ad3ebadafde0c86ee847ad07440
Sandeep Bangarh <sanbangarh309@gmail.com>
2019-02-02 22:30:35 +0530

    django admin changed

--- sandeep/migrations/0004_auto_20190202_1449.py
+++ sandeep/migrations/0004_auto_20190202_1449.py
@@ -10,6 +10,38 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=50)),
+                ('email', models.CharField(max_length=60)),
+                ('image', models.FileField(upload_to='static/img/projects/')),
+                ('about', models.CharField(max_length=255)),
+                ('password', models.CharField(max_length=100)),
+                ('address', models.CharField(max_length=255)),
+                ('phone', models.CharField(max_length=30)),
+                ('when', models.DateTimeField(auto_now_add=True, verbose_name='date created')),
+            ],
+        ),
+        migrations.DeleteModel(
+            name='Projects',
+        ),
+        migrations.DeleteModel(
+            name='Users',
+        ),
+        migrations.CreateModel(
+            name='Project',
+            fields=[
+                ('name', models.CharField(max_length=50)),
+                ('user_id', models.IntegerField()),
+                ('image', models.FileField(upload_to='static/img/projects/')),
+                ('about', models.CharField(max_length=255)),
+                ('start_date', models.DateField()),
+                ('end_date', models.DateField()),
+                ('when', models.DateTimeField(auto_now_add=True, verbose_name='date created')),
+            ],
+        ),
         migrations.AlterField(
             model_name='project',
             name='about',
